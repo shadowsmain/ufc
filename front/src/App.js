601d92159274f62fb30f6ca76fbc800cc4472ece
welcome <strifee322@gmail.com>
2021-09-12 21:51:09 +0500

    react custom page (homework)

---
+++ front/src/App.js
@@ -0,0 +1,29 @@
+import logo from './kenzo.gif';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          vladlen
+        </p>
+        <a
+          className="App-link"
+          href="https://vk.com/goticaaa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          me
+        </a>
+        <div class="header"></div>
+<p id="clock"></p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+var myVar = setInterval(function() {
+  myTimer();
+}, 1000);
+
+function myTimer() {
+  var d = new Date();
+  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
